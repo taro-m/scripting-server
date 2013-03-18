a2c7a44fd685218fe4888037263ddd3c6afdabeb
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2013-03-18 11:53:16 +0900

    impl new server framework

--- server/domain_test.js
+++ server/domain_test.js
@@ -1,22 +1,32 @@
 // vim:set ts=8 sts=4 sw=4 tw=0:
 
+var PORT = 8080;
+var DEBUG = true;
+
 var http = require('http');
 var domain = require('domain');
+var vm = require('vm');
 
 var server = http.createServer();
 server.addListener('request', handleRequest);
-server.listen(8080);
+server.listen(PORT);
+console.log('Server running at http://localhost:' + PORT + '/');
 
 function handleRequest(req, res) {
     var d = domain.create();
     d.add(req);
-    d.add(req);
-    d.on('error', function(er) { handleError(d, req, res, er); });
+    d.add(res);
+    var timeoutId = setTimeout(d.bind(function() {
+        handleTimeout(d, req, res)
+    }), 1000);
+
+    d.on('error', function(er) {
+        handleError(d, er, req, res);
+        clearTimeout(timeoutId);
+    });
+
     d.run(function() {
-        var timeoutId = setTimeout(d.bind(function() {
-            handleTimeout(d, req, res)
-        }), 1000);
-        main(d, req, res);
+        handleMain(d, req, res);
         clearTimeout(timeoutId);
     });
 }
@@ -24,7 +34,7 @@ function handleRequest(req, res) {
 function handleError(d, er, req, res) {
     try {
         res.writeHead(500);
-        res.end('Error occured, sorry.');
+        res.end('Error occured, sorry.: ' + er);
         res.on('close', function() {
             d.dispose();
         });
@@ -43,14 +53,30 @@ function handleTimeout(d, req, res) {
     });
 }
 
-function main(d, req, res) {
-    console.log('#main: enter');
-    while (true) {
+function execFunc(func) {
+    var r = func();
+    if ('next' in r) {
     }
+    return r;
+}
+
+var script = vm.createScript('while(true) { _loopHook(); }');
+
+function handleMain(d, req, res) {
+    console.log('#handleMain: #1');
+
+    script.runInNewContext({
+        '_loopHook': loopHook,
+    });
+    console.log('#handleMain: #2');
+
     res.writeHead(200);
     res.end('Completed.');
     res.on('close', function() {
         d.dipose();
     });
-    console.log('#main: leave');
+    console.log('#handleMain: #3');
+}
+
+function loopHook() {
 }
